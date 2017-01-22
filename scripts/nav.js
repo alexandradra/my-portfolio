57cf07824d028aaad2d95693d39f01b94c725134
Alexandradra <alexandra.dubost@gmail.com>
2017-01-22 20:54:48 +0100

    Fade in effect added to nav !

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-        var nav = document.getElementById('main-nav');
-        var btn = document.getElementById("open-nav");
-        var span = document.getElementsByClassName("close")[0];
-        btn.onclick = function() {
-            nav.style.display = "block";
-            btn.style.display = "none";
+    var nav = document.getElementById('main-nav');
+    var btn = document.getElementById("open-nav");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+        nav.style.display = "block";
+        btn.style.display = "none";
+        if (nav.classList.contains('is-paused')) {
+            nav.classList.remove('is-paused');
         }
-        span.onclick = function() {
+    }
+    span.onclick = function() {
+        nav.style.display = "none";
+        btn.style.display = "block";
+    }
+    document.onclick = function(event) {
+        if (event.target == nav) {
             nav.style.display = "none";
             btn.style.display = "block";
         }
-        document.onclick = function(event) {
-            if (event.target == nav) {
-                nav.style.display = "none";
-                btn.style.display = "block";
-            }
-        }
+    }
 });
